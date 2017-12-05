7220452777e3c2458e42b82b9c1dd681b85b6cc3
Ishak Khan <ishakkhan@Ishaks-MacBook-Pro.local>
2017-12-05 17:12:33 -0500

    first commit

---
+++ src/main/java/com/TalenTeach/app/App.java
@@ -0,0 +1,13 @@
+package com.TalenTeach.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
